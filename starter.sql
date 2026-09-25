4f5d3cdee3ece067ac6715a70ac8db8932e8f715
1U25CS009-ARASASELVAM <arasuselvam372@gmail.com>
2026-09-25 11:16:33 +0530

    Update starter.sql

--- starter.sql
+++ starter.sql
@@ -19,3 +19,15 @@ BEGIN
 
 END;
 /
+Assume 40 marks is the pass mark.
+SET SERVEROUTPUT ON;
+DECLARE
+    marks NUMBER := 65;
+BEGIN
+    IF marks >= 40 THEN
+        DBMS_OUTPUT.PUT_LINE('Student has Passed');
+    ELSE
+        DBMS_OUTPUT.PUT_LINE('Student has Failed');
+    END IF;
+END;
+/
